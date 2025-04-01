7f401af3a8f59e7ebe268ecba82d8736183d86dc
yunseo <jysjys2669@gmail.com>
2025-04-01 16:32:01 +0900

    like_posts의 다대다 설정, migrate

--- posts/models.py
+++ posts/models.py
@@ -13,6 +13,12 @@ class Post(models.Model):
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    # 좋아요를 누른 유저
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name = 'like_posts', #User와 Post를 M: N 연결
+    )
+
 class Comment(models.Model):
     content = models.CharField(max_length=200)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
