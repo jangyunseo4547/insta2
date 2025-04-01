ff5d0e55a190d84c1d2882e3cc170d5285384d9b
yunseo <jysjys2669@gmail.com>
2025-04-01 17:57:31 +0900

    profile 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns =[
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    # profile
+    path('<username>/', views.profile, name='profile'),
 ]
