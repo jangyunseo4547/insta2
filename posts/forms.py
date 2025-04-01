0c6f49e194b8eec965a22ee28cf46590ee45ab6f
yunseo <jysjys2669@gmail.com>
2025-04-01 16:24:11 +0900

    comment create, read 기능 구현

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,12 @@
-from .models import Post
+from .models import Post, Comment
 from django.forms import ModelForm
 
 class PostForm(ModelForm):
     class Meta():
         model = Post
         fields = ('image', 'content',)
+
+class CommentForm(ModelForm):
+    class Meta():
+        model = Comment
+        fields = ('content',)
